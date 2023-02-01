c50acd6735367b9c8f4a8bab6cee78c80b8019d9
Pranav Raj H <vinstarpranav@gmail.com>
2023-02-01 06:30:24 +0530

    new refined dataset

--- move.py
+++ move.py
@@ -3,13 +3,9 @@ from tqdm import tqdm
 from sqlalchemy import engine, create_engine
 
 engine = create_engine('mysql://pranav:password@localhost:3306/xo')
+rows = engine.execute("SELECT * FROM data1")
 
-
-with open("dataset.csv", "r") as file:
-    reader = csv.reader(file)
-    for row in tqdm(reader):
-        for i in range(9):
-            row[i] = int(float(row[i]))
-        row[9:] = [row[9:].index("1.0")]
-        query = "INSERT INTO data VALUES ({},{},{},{},{},{},{},{},{},{})".format(*row)
-        engine.execute(query)
+with open("/home/pranav/Documents/Projects/xo_ai/dataset3.csv", "w") as file:
+    writer = csv.writer(file)
+    for row in tqdm(rows):
+        writer.writerow(row)
