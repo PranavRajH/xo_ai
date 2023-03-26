2ab6d9d3f2900334acddccc742d86a5b1b791af1
Pranav Raj H <vinstarpranav@gmail.com>
2023-03-26 11:17:30 +0530

    added a node class to create tree data

--- mcst.py
+++ mcst.py
@@ -2,6 +2,18 @@ import numpy as np
 
 from TicTacToe import *
 
+class Node:
+    def __init__(self, state: np.array, player: int, parent = None, children: list = [], n: int = 1, w: int = 0, val: int = 0):
+        self.state = state
+        self.player = player
+        self.parent = parent
+        self.children = children
+        self.n = n
+        self.w = w
+        self.val = val
+
+    def __str__(self):
+        return f'state: \n{self.state}\nplayer: {self.player}\nn: {self.n}\nw: {self.w}\nval: {self.val}'
 
 def possible_moves(state: np.array) -> list:
     return [i for i in range(9) if state.reshape(-1)[i] == 0]
@@ -40,6 +52,37 @@ def walkdown(state: np.array, player: int, next_player: int, n: int, w: int, ver
             print(f'state: \n{new_state}\nplayer: {player}\nnext_player: {get_next_player(next_player)}\nn: {new_n}\nw: {new_w}\ncumulative n: {num}\ncumulative w: {wins}')
         print("out loop")
         return num, wins
+    
+def mcst_gen(root: Node, player: int, next_player: int) -> Node:
+    positions = possible_moves(root.state)
+    if len(positions) == 1:
+        if is_win(root.state, player, positions[0]):
+            root.parent.children.append(Node(np.copy(root.state), player, root, n=1, w=1, val=1))
+        else:
+            root.parent.children.append(Node(np.copy(root.state), player, root, n=1, w=0, val=0))
+    else:
+        for pos in positions:
+            if is_win(root.state, next_player, pos):
+                if next_player == player:
+                    root.children.append(Node(next_state(np.copy(root.state), next_player, pos), player, root, n=1, w=1, val=1))
+                else:
+                    root.children.append(Node(next_state(np.copy(root.state), next_player, pos), player, root, n=1, w=0, val=0))
+                continue
+            new_state = next_state(np.copy(root.state), next_player, pos)
+            child = Node(new_state, player, root)
+            child = mcst_gen(child, player, get_next_player(next_player))
+            root.children.append(child)
+    root.n += sum([child.n for child in root.children])
+    root.w += sum([child.w for child in root.children])
+    if root.parent:
+        total = sum([child.n for child in root.parent.children])
+    else:
+        total = 1
+    root.val = root.w / root.n + 0.5 * np.sqrt(2 * np.log(total) / root.n)
+    return root
+
+# print(walkdown(np.array([[1,0,-1],[1,-1,0],[0,1,-1]]), -1, -1, 1, 0, 1))
+# print(mcst_gen(Node(np.array([[1,0,-1],[1,-1,0],[0,1,-1]]), -1), -1, -1))
 
 """
     print(walkdown(np.zeros((3,3)), -1, -1, 1, 0))
