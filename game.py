7e491143bcd3bd7f5c50ba81ed6f15017e08b1f7
Pranav Raj H <vinstarpranav@gmail.com>
2023-04-02 17:59:23 +0530

    made game against mcst

--- game.py
+++ game.py
@@ -15,7 +15,31 @@ def get_next_best_move(state: np.array, player: int) -> int:
     for i in range(9):
         if res[i][0] != 0:
             vals[i] = res[i][1] / res[i][0] + np.sqrt(2 * np.log(total) / res[i][0])
+    print(vals)
     best_pos = np.argmax(vals)
     return best_pos
 
 # print(get_next_best_move(np.array([[1,0,-1],[1,-1,0],[0,1,-1]]), -1))
+
+if __name__ == '__main__':
+    while True:
+        board = new_board()
+        print(board)
+        player = 1
+        pos = int(input(f'{player}:'))
+        while not (is_win(board, player, pos) or is_draw(board)):
+            board = next_state(board, player, pos)
+            print(board)
+            player = get_next_player(player)
+            if player == 1:
+                pos = int(input(f'{player}:'))
+            else:
+                pos = get_next_best_move(board, player)
+                print(f'{player}: {pos}')
+        else:
+            if is_win(board, player, pos):
+                print(f'{player} wins')
+            else:
+                print('Draw')
+        if input(':') == 'q':
+            break
